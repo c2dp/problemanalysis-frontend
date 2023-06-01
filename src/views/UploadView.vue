2b5eead70e40b3936c4537c4600219b2301cdcd9
fcome <futurenear@163.com>
2023-06-01 23:43:35 +0800

    修改上传execel的前端代码

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -1,32 +1,62 @@
 <template>
-  <el-upload
-    ref="uploadRef"
-    class="upload-demo"
-    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-    :auto-upload="false"
-  >
-    <template #trigger>
-      <el-button type="primary">select file</el-button>
-    </template>
-
-    <el-button class="ml-3" type="success" @click="submitUpload">
-      upload to server
-    </el-button>
-
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500kb
-      </div>
-    </template>
-  </el-upload>
+  <div>
+    <el-upload
+      class="upload-demo"
+      action="/api/upload"
+      :before-upload="handleBeforeUpload"
+      :on-success="handleUploadSuccess"
+      :on-error="handleUploadError"
+    >
+      <el-button type="primary">点击上传</el-button>
+    </el-upload>
+  </div>
 </template>
-<script lang="ts" setup>
-import { ref } from "vue";
-import type { UploadInstance } from "element-plus";
 
-const uploadRef = ref<UploadInstance>();
+<script lang="ts">
+import { defineComponent } from "vue";
 
-const submitUpload = () => {
-  uploadRef.value!.submit();
-};
+import { ElUpload, ElButton, ElMessage } from "element-plus";
+
+import axios from "axios";
+
+export default defineComponent({
+  components: {
+    ElUpload,
+
+    ElButton,
+  },
+
+  methods: {
+    handleBeforeUpload(file: File) {
+      // 只允许上传 xlsx 文件
+      const extension: string | undefined = file.name.split(".").pop();
+
+      const allowedExtensions = ["xlsx"];
+
+      if (!allowedExtensions.includes(extension ?? "")) {
+        ElMessage.error("只允许上传 xlsx 文件");
+
+        return false;
+      }
+
+      return true;
+    },
+
+    handleUploadSuccess(response: any) {
+      // 处理上传成功的逻辑
+
+      const { tableName } = response.data ?? "";
+
+      ElMessage.success(`表格 ${tableName} 上传成功`);
+    },
+
+    handleUploadError(error: any) {
+      // 处理上传失败的逻辑
+
+      console.error(error);
+
+      ElMessage.error("上传失败，请重试");
+    },
+  },
+});
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,20 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8081/", //接口域名
+        changeOrigin: true, //是否跨域
+        ws: false, //是否代理 websockets
+        secure: false, //是否https接口
+        pathRewrite: {
+          //路径重置
+          "^/api": "/api",
+        },
+      },
+    },
+  },
+};
